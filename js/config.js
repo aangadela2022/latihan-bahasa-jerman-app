5b8ec184563e01209ca6328e89f2370d44e5fdf4
aangadela2022 <aangadela2022@users.noreply.github.com>
2026-04-02 15:12:24 +0700

    Stop tracking js/config.js to protect API keys

--- js/config.js
+++
@@ -1,8 +0,0 @@
-// Ini adalah CONTOH file konfigurasi.
-// Jika Anda meng-clone / mendownload repository ini pertama kali,
-// silakan duplikat/copy file ini, lalu ubah namanya menjadi "config.js"
-// dan ubah nilainya dengan API Key milik Anda sendiri.
-
-window.CONFIG = {
-    GEMINI_API_KEY: "AIzaSyDRivxMHx3kClXXScDK1oQovRv8ZGSHcW0"
-};
